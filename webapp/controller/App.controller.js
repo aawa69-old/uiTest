5adf29aead48096a485d74e7c360ce9f0df1e526
aawa69 <aawa69@users.noreply.github.com>
2017-05-10 18:46:00 +0800

    direct

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -17,7 +17,7 @@ sap.ui.define([
 				// read msg from i18n model
 				var oBundle = this.getView().getModel("i18n").getResourceBundle();
 				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
-				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				var sMsg = oBundle.getText("helloMsg2", [sRecipient]);
 
 				// show message
 				MessageToast.show(sMsg);
